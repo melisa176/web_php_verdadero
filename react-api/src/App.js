c24438797735742b96550fc673f41fa19617c78b
melisa176 <carolinacastillon27@gmail.com>
2025-03-04 23:54:55 -0500

    nu

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
+
+import Productos from './components/Productos';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <BrowserRouter>
+
+      <Routes>
+
+     
+
+        <Route path='/' element={<Productos/>}></Route>
+
+   
+
+      </Routes>
+
+    </BrowserRouter>
+
+  )
+
 }
+
 
 export default App;
--- start-all.bat
+++ start-all.bat
@@ -1,12 +1,14 @@
 @echo off
 title Iniciando Proyectos React
-echo Iniciando React_practica...
-start cmd /k "cd /d %~dp0React_nuevo\app-first && npm start"
+
+echo Iniciando React_practica en puerto 3000...
+start cmd /k "cd /d %~dp0React_nuevo\app-first && set PORT=3002 && npm start"
+
 
 timeout /t 3
 
-echo Iniciando React-API...
-start cmd /k "cd /d %~dp0react-api && npm start"
+echo Iniciando React-API en puerto 3001...
+start cmd /k "cd /d %~dp0react-api && set PORT=3001 && npm start"
 
 echo Todos los proyectos han sido iniciados.
 pause
